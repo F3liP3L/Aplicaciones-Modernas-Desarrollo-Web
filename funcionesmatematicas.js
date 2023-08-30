feed568451f290a38658c5ec73c4598449551622
juan.cardona <felipe7u7432@gmail.com>
2023-08-30 01:19:00 -0500

    Agrega funciones matematicas y expresiones regulares en funcionestexto.js  (se retoma el semillero 🎉)

--- funcionesmatematicas.js
+++ funcionesmatematicas.js
@@ -15,3 +15,13 @@ function multiply(array){
 function division(array){
     return array.reduce((acumulador, valorActual) => acumulador / valorActual);
 }
+
+function convertNumber(value) {
+    if(isInt(value)) {
+        return parseInt(value, 10);
+    } else if(isDecimal(value)) {
+        return parseFloat(value);
+    } else {
+        return 0;
+    }
+}
